0a6dbc65ffce71f6549261dc1c7ab94936f61537
Emmanuel Mark <info@emmanuelmark.guru>
2017-12-20 16:16:03 -0500

    first real commit

---
+++ src/downloadify.js
@@ -0,0 +1,42 @@
+(function(){
+  class Downloadify {
+  constructor(target, props){
+    this.target = target
+    this.windows = props.windows
+    this.macos = props.macos
+    this.linux = props.linux
+    this.click = this.click.bind(this)
+    this.kill = this.kill.bind(this)
+
+    if(navigator.platform.indexOf('Windows NT') != -1) {
+      const platform = 'windows'
+    } else if(navigator.platform.indexOf('Mackintosh') != -1) {
+      const platform = 'windows'
+    } else if(navigator.platform.indexOf('Linux') != -1) {
+      const platform = 'linux'
+    }
+
+    this.target.addEventListener('click', this.click())
+  }
+  kill() {
+    this.target.removeEventListener('click', this.click())
+    return 'Downloadify removed'
+  }
+  private click() {
+    switch(this.platform) {
+      case 'windows':
+        return window.location = this.windows
+      case 'macos':
+        return window.location = this.macos
+      case 'linux':
+        return window.location = this.linux
+      default:
+        if(this.callback) {
+          return window.location = this.callback
+        } else {
+          throw new Error
+        }
+    }
+  }
+}
+})
