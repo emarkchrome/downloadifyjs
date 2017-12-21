ad27d87c9db7711e4695fa78f8cd67d81917532a
Emmanuel Mark <info@emmanuelmark.guru>
2017-12-20 20:59:43 -0500

    gulp script: adds *.min suffix to file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-gulp.task('default', function() {
-    return gulp.src('src/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('dist'));
+gulp.task('default', ['lint'], function() {
+  return gulp.src('src/*.js')
+    .pipe(babel())
+    .pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+    .pipe(gulp.dest('dist'));
 });
+
+gulp.task('build', ['lint'], function() {
+	return gulp.src('src/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('dist'));
+})
+
+gulp.task('lint', function() {
+  return gulp.src('src/*.js')
+    .pipe(eslint({ fix: true }))
+    .pipe(eslint.format());
+})
